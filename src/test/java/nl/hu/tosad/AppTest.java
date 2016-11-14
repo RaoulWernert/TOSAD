ad58521e692f109e219e91e9e7c07ed129ba96d0
Johan de Graaf <johan@mcpvp.eu>
2016-11-14 19:54:11 +0100

    initial commit

---
+++ src/test/java/nl/hu/tosad/AppTest.java
@@ -0,0 +1,14 @@
+package nl.hu.tosad;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+    @org.junit.Test
+    public void main() throws Exception {
+        assertTrue( true );
+    }
+}
