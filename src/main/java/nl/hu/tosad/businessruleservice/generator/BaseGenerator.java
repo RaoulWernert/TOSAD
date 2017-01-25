fcc3d6742d7503030f54c7742f8d55212c660537
Johan de Graaf <johan@mcpvp.eu>
2017-01-25 18:48:27 +0100

    triggers

--- src/main/java/nl/hu/tosad/businessruleservice/generator/BaseGenerator.java
+++ src/main/java/nl/hu/tosad/businessruleservice/generator/BaseGenerator.java
@@ -9,40 +9,67 @@ import nl.hu.tosad.businessruleservice.model.rules.*;
 public class BaseGenerator implements IGenerator {
     @Override
     public String generateDDL(AttributeRangeRule rule) {
-        return new ConstraintBuilder().newConstraint(rule.getName())
-                .onTable(rule.getTable())
-                .onColumn(rule.getAttribute())
-                .addBetween(rule.getMin(), rule.getMax())
-                .build();
+        switch(rule.getImplementation()) {
+            case CONSTRAINT:
+                return new ConstraintBuilder().newConstraint(rule.getName())
+                        .onTable(rule.getTable())
+                        .onColumn(rule.getAttribute())
+                        .addBetween(rule.getMin(), rule.getMax())
+                        .build();
+            case TRIGGER:
+                return "";
+            default:
+                return null;
+        }
     }
 
     @Override
     public String generateDDL(AttributeCompareRule rule) {
-        return new ConstraintBuilder().newConstraint(rule.getName())
-                .onTable(rule.getTable())
-                .onColumn(rule.getAttribute())
-                .addComparisonOperator(rule.getComparisonOperator())
-                .addValue(rule.getValue())
-                .build();
+        switch(rule.getImplementation()) {
+            case CONSTRAINT:
+                return new ConstraintBuilder().newConstraint(rule.getName())
+                        .onTable(rule.getTable())
+                        .onColumn(rule.getAttribute())
+                        .addComparisonOperator(rule.getComparisonOperator())
+                        .addValue(rule.getValue())
+                        .build();
+            case TRIGGER:
+                return "";
+            default:
+                return null;
+        }
     }
 
     @Override
     public String generateDDL(AttributeListRule rule) {
-        return new ConstraintBuilder().newConstraint(rule.getName())
-                .onTable(rule.getTable())
-                .onColumn(rule.getAttribute())
-                .addOperators(rule.getLogicalOperator(), rule.getComparisonOperator())
-                .addValues(rule.getValues())
-                .build();
-
+        switch(rule.getImplementation()) {
+            case CONSTRAINT:
+                return new ConstraintBuilder().newConstraint(rule.getName())
+                        .onTable(rule.getTable())
+                        .onColumn(rule.getAttribute())
+                        .addOperators(rule.getLogicalOperator(), rule.getComparisonOperator())
+                        .addValues(rule.getValues())
+                        .build();
+            case TRIGGER:
+                return "";
+            default:
+                return null;
+        }
     }
 
     @Override
     public String generateDDL(AttributeOtherRule rule) {
-        return new ConstraintBuilder().newConstraint(rule.getName())
-                .onTable(rule.getTable())
-                .onColumn(rule.getAttribute())
-                .addStatement(rule.getStatement())
-                .build();
+        switch(rule.getImplementation()) {
+            case CONSTRAINT:
+                return new ConstraintBuilder().newConstraint(rule.getName())
+                        .onTable(rule.getTable())
+                        .onColumn(rule.getAttribute())
+                        .addStatement(rule.getStatement())
+                        .build();
+            case TRIGGER:
+                return "";
+            default:
+                return null;
+        }
     }
 }
