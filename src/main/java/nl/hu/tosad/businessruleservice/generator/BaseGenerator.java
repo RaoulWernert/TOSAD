620e4ffd4994d00b92dee3871209458c3f28959e
RaoulWernert <raoulwernert@gmail.com>
2017-01-29 00:14:22 +0100

    Op hoop van zegen

--- src/main/java/nl/hu/tosad/businessruleservice/generator/BaseGenerator.java
+++ src/main/java/nl/hu/tosad/businessruleservice/generator/BaseGenerator.java
@@ -3,7 +3,8 @@ package nl.hu.tosad.businessruleservice.generator;
 import nl.hu.tosad.businessruleservice.generator.constraintbuilder.ConstraintBuilder;
 import nl.hu.tosad.businessruleservice.generator.triggerbuilder.TriggerBuilder;
 import nl.hu.tosad.businessruleservice.model.rules.*;
-import nl.hu.tosad.businessruleservice.persistance.target.OracleTargetDAO;
+
+import java.util.List;
 
 /**
  * Created by Raoul on 1/21/2017.
@@ -19,12 +20,12 @@ public class BaseGenerator implements IGenerator {
                         .addBetween(rule.getMin(), rule.getMax())
                         .build();
             case TRIGGER:
-                return TriggerBuilder.newTrigger(rule.getName())
-                        .onRuleType(rule.getRuleType())
-                        .addEvent(rule.getTable(), rule.getAttribute())
-                        .addColumn(rule.getAttribute())
+                return new TriggerBuilder(rule.getName())
+                        .setTable(rule.getTable())
+                        .setEvents(rule.getRuleType())
+                        .setColumns(rule.getAttribute())
                         .addBetween(rule.getMin(), rule.getMax())
-                        .addErrorMessage(rule.getErrormsg())
+                        .setError(rule.getErrormsg())
                         .build();
             default:
                 return null;
@@ -42,13 +43,12 @@ public class BaseGenerator implements IGenerator {
                         .addValue(rule.getValue())
                         .build();
             case TRIGGER:
-                return TriggerBuilder.newTrigger(rule.getName())
-                        .onRuleType(rule.getRuleType())
-                        .addEvent(rule.getTable(), rule.getAttribute())
-                        .addColumn(rule.getAttribute())
-                        .addComparisonOperator(rule.getComparisonOperator())
-                        .addValue(rule.getValue())
-                        .addErrorMessage(rule.getErrormsg())
+                return new TriggerBuilder(rule.getName())
+                        .setTable(rule.getTable())
+                        .setEvents(rule.getRuleType())
+                        .setColumns(rule.getAttribute())
+                        .addComparison(rule.getAttribute(), rule.getComparisonOperator(), rule.getValue())
+                        .setError(rule.getErrormsg())
                         .build();
             default:
                 return null;
@@ -66,13 +66,12 @@ public class BaseGenerator implements IGenerator {
                         .addValues(rule.getValues())
                         .build();
             case TRIGGER:
-                return TriggerBuilder.newTrigger(rule.getName())
-                        .onRuleType(rule.getRuleType())
-                        .addEvent(rule.getTable(), rule.getAttribute())
-                        .addColumn(rule.getAttribute())
-                        .addOperators(rule.getLogicalOperator(), rule.getComparisonOperator())
-                        .addValues(rule.getValues())
-                        .addErrorMessage(rule.getErrormsg())
+                return new TriggerBuilder(rule.getName())
+                        .setTable(rule.getTable())
+                        .setEvents(rule.getRuleType())
+                        .setColumns(rule.getAttribute())
+                        .addComparisons(rule.getAttribute(), rule.getLogicalOperator(), rule.getComparisonOperator(), getValuesFromList(rule.getValues()))
+                        .setError(rule.getErrormsg())
                         .build();
             default:
                 return null;
@@ -89,11 +88,12 @@ public class BaseGenerator implements IGenerator {
                         .addStatement(rule.getStatement())
                         .build();
             case TRIGGER:
-                return TriggerBuilder.newTrigger(rule.getName())
-                        .onRuleType(rule.getRuleType())
-                        .addEvent(rule.getTable(), rule.getAttribute())
-                        .addStatement(rule.getStatement())
-                        .addErrorMessage(rule.getErrormsg())
+                return new TriggerBuilder(rule.getName())
+                        .setTable(rule.getTable())
+                        .setEvents(rule.getRuleType())
+                        .setColumns(rule.getAttribute())
+                        .setStatement(rule.getStatement())
+                        .setError(rule.getErrormsg())
                         .build();
             default:
                 return null;
@@ -110,13 +110,12 @@ public class BaseGenerator implements IGenerator {
                         .addOperator(rule.getOperator())
                         .build();
             case TRIGGER:
-                return TriggerBuilder.newTrigger(rule.getName())
-                        .onRuleType(rule.getRuleType())
-                        .addEvent(rule.getTable(), rule.getColumn1(), rule.getColumn2())
-                        .addColumn(rule.getColumn1())
-                        .addComparisonOperator(rule.getOperator())
-                        .addSecondColumn(rule.getColumn2())
-                        .addErrorMessage(rule.getErrormsg())
+                return new TriggerBuilder(rule.getName())
+                        .setTable(rule.getTable())
+                        .setEvents(rule.getRuleType())
+                        .setColumns(rule.getColumn1())
+                        .addComparison(rule.getColumn1(), rule.getOperator(), rule.getColumn2())
+                        .setError(rule.getErrormsg())
                         .build();
             default:
                 return null;
@@ -132,11 +131,11 @@ public class BaseGenerator implements IGenerator {
                         .addStatement(rule.getStatement())
                         .build();
             case TRIGGER:
-                return TriggerBuilder.newTrigger(rule.getName())
-                        .onRuleType(rule.getRuleType())
-                        .addEvent(rule.getTable(), rule.getColumn1(), rule.getColumn2())
-                        .addStatement(rule.getStatement())
-                        .addErrorMessage(rule.getErrormsg())
+                return new TriggerBuilder(rule.getName())
+                        .setTable(rule.getTable())
+                        .setEvents(rule.getRuleType())
+                        .setStatement(rule.getStatement())
+                        .setError(rule.getErrormsg())
                         .build();
             default:
                 return null;
@@ -145,22 +144,36 @@ public class BaseGenerator implements IGenerator {
 
     @Override
     public String generateDDL(InterEntityCompareRule rule) {
-        return null;
+        /*String trigger1 = TriggerBuilder.newTrigger(rule.getName())
+                .onRuleType(rule.getRuleType())
+                .addEvent(rule.getTable(), rule.getColumn())*/
+        return "WIP";
     }
 
     @Override
     public String generateDDL(EntityOtherRule rule) {
-        return TriggerBuilder.newTrigger(rule.getName())
+        /*return TriggerBuilder.newTrigger(rule.getName())
                 .onRuleType(rule.getRuleType())
                 .addEvent(rule.getTable(), rule.getColumn(), rule.getColumn2())
                 .addCodeBlock(rule.getStatement())
                 .addAllColumns(new OracleTargetDAO().getColumns(rule.getTarget(), rule.getTable())) //TODO: Columns op andere manier ophalen
                 .addErrorMessage(rule.getErrormsg())
-                .build();
+                .build();*/
+        return "WIP";
     }
 
     @Override
     public String generateDDL(ModifyRule rule) {
-        return null;
+        return "WIP";
+    }
+
+    private String getValuesFromList(List<String> list) {
+        String values = "";
+        for (String str : list) {
+            if (str.length() > 0) {
+                values += "'" + str + "',";
+            }
+        }
+        return values.substring(0, values.length() - 1);
     }
 }
