5d0a33470453bfd1bcfc4e2e9900ec8112192f7d
Johan de Graaf <johan@mcpvp.eu>
2017-01-29 17:27:50 +0100

    getprimarykey getforeignkey

--- src/main/java/nl/hu/tosad/businessruleservice/controller/IController.java
+++ src/main/java/nl/hu/tosad/businessruleservice/controller/IController.java
@@ -8,6 +8,8 @@ import java.util.List;
 public interface IController {
     List<String> getTables(TargetDatabase target);
     List<String> getColumns(TargetDatabase target, String tablename);
+    String getPrimaryKey(TargetDatabase target, String tablename);
+    String getForeignKey(TargetDatabase target, String targetTable, String referencingTable);
     void implement(String query, BusinessRule rule);
     void delete(BusinessRule rule);
 }
