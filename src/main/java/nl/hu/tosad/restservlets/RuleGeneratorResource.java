1fdd2387e5d711398913374744683b31fd4f3067
Johan de Graaf <johan@mcpvp.eu>
2017-02-01 00:22:50 +0100

    error page + logging

--- src/main/java/nl/hu/tosad/restservlets/RuleGeneratorResource.java
+++ src/main/java/nl/hu/tosad/restservlets/RuleGeneratorResource.java
@@ -88,7 +88,7 @@ public class RuleGeneratorResource {
     @Produces("text/plain")
     public String serverCheck(@QueryParam("targetid") int targetid) {
         boolean response = BusinessRuleService.getInstance().checkTarget(targetid);
-        logger.Log(String.format("CheckTarget  | req: %d | resp: %s", targetid, " = "+(response?"online":"offline")));
+        logger.Log(String.format("CheckTarget  | req: %d | resp: %s", targetid, targetid+" = "+(response?"online":"offline")));
         return response?"1":"0";
     }
 
